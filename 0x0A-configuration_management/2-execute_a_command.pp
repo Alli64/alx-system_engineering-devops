4efcca7dc8ac4fc46547d0331d222844f6d9fb89
Alli64 <dushimealliance1@gmail.com>
2023-11-24 05:54:52 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,4 +1,5 @@
 #create a manifest that kills a process named killmenow
-exec {'pkill':
-command => 'pkill killmenow',
-provider => 'shell',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
